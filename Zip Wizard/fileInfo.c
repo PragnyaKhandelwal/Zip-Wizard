d5fec3ef05ec379463f8f67a895e31bf58a8cb13
Pratishtha Singh <pratishtha.singh45@gmail.com>
2024-10-30 13:27:09 +0530

    Update fileInfo.c

--- Zip Wizard/fileInfo.c
+++ Zip Wizard/fileInfo.c
@@ -17,11 +17,7 @@ void fileinfo()
     {
         zwPrint("Error: File name is too long.\n", 20, ERROR_FILE);
     }
-    if (strstr(file_name, ".txt") == NULL)
-    {
-        zwPrint("Error: File name must end with .txt extension.\n", 20, ERROR_FILE);
-        return;
-    }
+   
     if (strlen(file_name) == 0)
     {
         zwPrint("Error: File name cannot be empty.\n", 20, ERROR_FILE);
