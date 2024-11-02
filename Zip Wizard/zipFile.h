a5ad371a179a88d30221e3486ccb8b3fb009d538
Pragnya Khandelwal <prag1704@gmail.com>
2024-11-02 11:36:24 +0530

    Update zipFile.h

--- Zip Wizard/zipFile.h
+++ Zip Wizard/zipFile.h
@@ -1,6 +1,8 @@
 #ifndef ZIPFILE_H
 #define ZIPFILE_H
 
+#define MAX_FILE_NAME_LENGTH 255
+#define MAX_TREE_NODES 256
 #define WINDOW_SIZE 4096
 #define BUFFER_SIZE 18
 
@@ -15,23 +17,18 @@ typedef struct Node {
 // Min-heap structure
 typedef struct MinHeap {
     unsigned size;
-    Node *array[MAX_TREE_NODES];
+    Node** array;
 } MinHeap;
 
 void lz77(FILE *inputFile, FILE *compressedFile);
-void huffmanCoding(const char *text, char codes[MAX_TREE_NODES][MAX_TREE_NODES]);
+void huffmanCoding(FILE *lz77Output, const char *compressedFile);
 Node* createNode(char character, unsigned frequency);
 MinHeap* createMinHeap();
 void insertMinHeap(MinHeap* minHeap, Node* node);
 Node* extractMin(MinHeap* minHeap);
-void buildHuffmanTree(const char *text, char codes[MAX_TREE_NODES][MAX_TREE_NODES]);
+void buildHuffmanTree(unsigned frequency[], Node** root);
 void generateHuffmanCodes(Node* root, char *code, int depth, char codes[MAX_TREE_NODES][MAX_TREE_NODES]);
 void freeHuffmanTree(Node* root);
-
-int outputCount = 0;
-char outputLog[MAX_OUTPUT_LINES][100];
-
-// Function to zip a file
 void zipfile();
 
 #endif // ZIPFILE_H
