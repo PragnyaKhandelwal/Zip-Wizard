07f694d4aef12adadc589891bbe76ef1654cdcc2
Pratishtha Singh <182654574+Pratishtha5@users.noreply.github.com>
2024-12-17 19:54:51 +0530

    Update unzipFile.h

--- Zip Wizard/unzipFile.h
+++ Zip Wizard/unzipFile.h
@@ -1,14 +1,12 @@
 // unzip.h
 #ifndef UNZIP_H
 #define UNZIP_H
+
 #include <stdio.h>
-# include "Utils.h"
+#include "Utils.h"
 #include "zipFile.h"  // Ensure this is included for Node type and other declarations
 
-void unzipFile(); // No parameters now
-void lz77Decompress(FILE *compressedFile, FILE *outputFile);
-void huffmanDecode(FILE *huffmanEncodedFile, FILE *outputFile, Node *root);
-Node *buildHuffmanTreeFromCodes(FILE *huffmanCodesFile);
+void unzipFile(); // Function to unzip a file
+void lz77Decompress(FILE *compressedFile, FILE *outputFile); // LZ77 decompression
 
 #endif // UNZIP_H
-
